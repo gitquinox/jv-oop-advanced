8d47b4e2ccf2500d8fcc3358a6f6ad849c3d9df6
gitquinox <maksumen18@gmail.com>
2026-04-06 17:32:44 +0200

    Created variables for random number and default radius and also for figure count

--- src/main/java/core/basesyntax/FigureSupplier.java
+++ src/main/java/core/basesyntax/FigureSupplier.java
@@ -5,23 +5,25 @@ import java.util.Random;
 public class FigureSupplier {
     private final ColorSupplier colorSupplier = new ColorSupplier();
     private final Random random = new Random();
-    private final int RANDOM_NUMBER = random.nextInt(10) + 1;
+    private final int randomNumber = random.nextInt(10) + 1;
+    private final int figureCount = 5;
+    private final int defaultRadius = 10;
 
     public Figure getRandomFigure() {
-        int figureType = random.nextInt(5);
+        int figureType = random.nextInt(figureCount);
         String randomColor = colorSupplier.getRandomColor();
 
         return switch (figureType) {
-            case 0 -> new Circle(randomColor, RANDOM_NUMBER);
-            case 1 -> new Square(randomColor, RANDOM_NUMBER);
-            case 2 -> new RightTriangle(randomColor, RANDOM_NUMBER, RANDOM_NUMBER);
-            case 3 -> new Rectangle(randomColor, RANDOM_NUMBER, RANDOM_NUMBER);
+            case 0 -> new Circle(randomColor, randomNumber);
+            case 1 -> new Square(randomColor, randomNumber);
+            case 2 -> new RightTriangle(randomColor, randomNumber, randomNumber);
+            case 3 -> new Rectangle(randomColor, randomNumber, randomNumber);
             default -> new IsoscelesTrapezoid(randomColor,
-                    RANDOM_NUMBER, RANDOM_NUMBER, RANDOM_NUMBER, RANDOM_NUMBER);
+                    randomNumber, randomNumber, randomNumber, randomNumber);
         };
     }
 
     public Figure getDefaultFigure() {
-        return new Circle(Colors.WHITE.getName(), 10);
+        return new Circle(Colors.WHITE.getName(), defaultRadius);
     }
 }
